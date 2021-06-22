8ade1a211eae465351b0a725d67074baeffe5c5d
Kiyan Gauss <fourcels@gmail.com>
2021-06-22 11:08:55 +0800

    Initial Commit

---
+++ Shared/WangYiNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WangYiNewsApp.swift
+//  Shared
+//
+//  Created by Kiyan Gauss on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct WangYiNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
